ca7e385e2806fc23db3c42aeeb20125b9006b12d
abdi <debelaabdi3@gmail.com>
2024-07-24 08:36:17 +0300

    phone book changes

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
-import Filter from "./components/filter";
-import PersonForm from "./components/personForm";
-import Persons from "./components/person";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Filter from './components/filter';
+import PersonForm from './components/personForm';
+import Persons from './components/person';
 
 const App = () => {
+  // State for notes
+  const [notes, setNotes] = useState([]);
+
+  // State for persons
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
@@ -12,16 +18,36 @@ const App = () => {
   ]);
   const [newFilter, setNewFilter] = useState('');
 
+  // Fetch notes data from server
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data);
+        console.log(response.data);
+      });
+  }, []);
+  console.log('render', persons.length, 'persons');
+
+  // Handle filter change
   const handleFilterChange = (filterValue) => {
     setNewFilter(filterValue);
   };
 
+  // Add a new person
   const addPerson = (newPerson) => {
     setPersons([...persons, newPerson]);
   };
 
   return (
     <div>
+      <h2>Notes</h2>
+      {notes.map(note => (
+        <Note key={note.id} note={note} />
+      ))}
+
       <h2>Phonebook</h2>
 
       {/* Filter component */}
